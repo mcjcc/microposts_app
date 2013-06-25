581373a294db56915143527c265d366e4ccced93
Johnny Chen <jcc0821@gmail.com>
2013-06-25 12:05:42 -0400

    added smtp config to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,20 @@ MicropostsApp::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME_DEV"],
+  password: ENV["GMAIL_PASSWORD_DEV"]
+}
+
+config.action_mailer.default_url_options = { :host => 'microposts-application.herokuapp.com' }
+
   # Enable threaded mode
   # config.threadsafe!
 
